d02e0627391ada6ead1b3a54dca38892f3834cb0
Joseba <joseba.merino.pina@gmail.com>
2020-01-21 09:37:47 +0100

    Initial commit

--- src/main/java/com/ipartek/formacion/supermercado/modelo/dao/ICategoriaDAO.java
+++
@@ -1,7 +0,0 @@
-package com.ipartek.formacion.supermercado.modelo.dao;
-
-import com.ipartek.formacion.supermercado.modelo.pojo.Categoria;
-
-public interface ICategoriaDAO extends IDAO<Categoria> {
-
-}
